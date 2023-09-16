8975c14a269adb7c42173f02184c7b14bea3bcf0
Mikhail Rubanov <akaDuality@yandex.ru>
2023-09-16 13:31:17 -0300

    Accessibility over UI

--- A11yReuse/ViewController.swift
+++ A11yReuse/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        50
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+        
+        if indexPath.row == 0 {
+            cell.text = "Header \(indexPath.section)"
+            cell.accessibilityTraits = [.header]
+        } else {
+            cell.text = "Section \(indexPath.section), row \(indexPath.row)"
+            cell.accessibilityTraits = [.staticText]
+        }
+        
+        return cell
+    }
 }
 
+class Cell: UICollectionViewCell {
+    @IBOutlet private var titleLabel: UILabel!
+    
+    var text: String = "" {
+        didSet {
+            titleLabel.text = text
+            accessibilityLabel = text
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        isAccessibilityElement = true
+    }
+}
