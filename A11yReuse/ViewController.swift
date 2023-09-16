1c5fe6a3ddc4753f0ffc178221883d0f0a1b3d3c
Mikhail Rubanov <akaDuality@yandex.ru>
2023-09-16 13:21:35 -0300

    Initial Commit

---
+++ A11yReuse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A11yReuse
+//
+//  Created by Mikhail Rubanov on 16.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
